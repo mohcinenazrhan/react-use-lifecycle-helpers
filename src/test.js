049df79229851c48c31436ca4e3e77c5f3e25e35
Mohcine NAZRHAN <nazrhan.mohcine@gmail.com>
2020-02-15 22:57:03 +0100

    chore (test): create test workflow for useLifecycleHelpers

--- src/test.js
+++ src/test.js
@@ -1,7 +1,41 @@
-import ExampleComponent from './'
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+import {
+  render,
+  fireEvent,
+  cleanup,
+  queryByAttribute
+} from '@testing-library/react'
+import useLifecycleHelpers from './'
 
-describe('ExampleComponent', () => {
-  it('is truthy', () => {
-    expect(ExampleComponent).toBeTruthy()
-  })
+afterEach(cleanup)
+
+const Component = props => {
+  const [componentState, setComponentState] = useState(null)
+  const [state, setState] = useState({ counter: 0 })
+
+  const {
+    useComponentDidMount,
+    useComponentDidUpdate,
+    useComponentWillUnmount,
+    useOnDependenciesChange
+  } = useLifecycleHelpers(state, props)
+
+  return (
+    <div id='component'>
+      <p>{componentState}</p>
+      <button onClick={() => setState({ counter: 1 })}>Update state</button>
+    </div>
+  )
+}
+
+Component.propTypes = {
+  data: PropTypes.string
+}
+
+function renderComponent(props) {
+  return render(<Component {...props} />)
+}
+
+describe('Test useLifecycleHelpers custom hook', () => {
 })
